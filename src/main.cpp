8e663261207f46fa29c8b0394fd91d8752bad6d4
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2024-03-14 11:10:23 -0500

    Add rompath argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <string>
+#include "chip8.hpp"
 
-int main() {
-  std::cout << "Hello World!" << std::endl;
+int main(int argc, char* argv[]) {
+  std::cout << "There are " << argc << " arguments\n";
+  std::string romPath;
+  // Leaving max arguments at 1 until arguments other than the rom path are needed
+  for (int i=1; i < 2; i++) {
+    romPath = std::string(argv[i]);
+    std::cout << romPath << std::endl;
+  }
 
+  Chip8 chip8 = Chip8(romPath);
   return 0;
 }
