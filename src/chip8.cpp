bfa164f330b6af25e3ad42aea677dfd251b73fd5
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2024-03-15 13:35:25 -0500

    Add member initialization list

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,24 +1,35 @@
 #include "chip8.hpp"
 
-Chip8::Chip8(std::string romPath) {
+Chip8::Chip8(std::string romPath) : memory{0},
+                                    displayBuffer{0},
+                                    counter{512},
+                                    soundRegister{0},
+                                    delayRegister{0},
+                                    V{0},
+                                    I{0}
+{
     std::streampos size;
-    char * romBuffer;
+    char *romBuffer;
     std::ifstream romFile;
     romFile.open(romPath, std::ios::ate | std::ios::binary | std::ios::in);
     // first 512 bytes are reserved for the interpreter itself
-    if (romFile.is_open()) {
+    if (romFile.is_open())
+    {
         size = romFile.tellg();
-        romBuffer = new char [size];
+        romBuffer = new char[size];
         romFile.seekg(0, std::ios::beg);
         romFile.read(romBuffer, size);
         romFile.close();
 
         std::cout << "Read " << romPath << std::endl;
         // read romBuffer into program memory
-        for (int i=0; i<size; i++) {
-            memory[i+512] = romBuffer[i];
+        for (int i = 0; i < size; i++)
+        {
+            memory[i + 512] = romBuffer[i];
         }
-    } else {
+    }
+    else
+    {
         std::cerr << "Failed to read rom file!" << std::endl;
     }
 }
