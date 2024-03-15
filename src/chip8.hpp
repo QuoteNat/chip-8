7782497cde21ac2f2f2904e779322f77fdbf2af5
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2024-03-15 13:35:00 -0500

    Add registers, stack, and counter

--- src/chip8.hpp
+++ src/chip8.hpp
@@ -4,22 +4,33 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <stack>
 
 /**
  * Represents a Chip-8 interpreter
-*/
-class Chip8 {
-    private:
-        // Chip 8 RAM
-        unsigned char memory[4096];
-        bool displayBuffer[64*32];
-        // REGISTERS
-        unsigned char soundRegister;
-        unsigned char delayRegister;
-    public:
-        /**
-         * Initializes a chip8 interpreter with a given rom
-         * @param rom The path to the chip8 rom to be run.
-        */
-        Chip8(std::string romPath);
+ */
+class Chip8
+{
+private:
+    // Chip 8 RAM
+    unsigned char memory[4096];
+    bool displayBuffer[64 * 32];
+    // Stores addresses that subroutines return to after completing
+    std::stack<short> stack;
+    // Program counter for the currently executing address
+    short counter;
+    // REGISTERS
+    unsigned char soundRegister;
+    unsigned char delayRegister;
+    // general purpose registers
+    unsigned char V[16];
+    // generally used for memory addresses
+    short I;
+
+public:
+    /**
+     * Initializes a chip8 interpreter with a given rom
+     * @param rom The path to the chip8 rom to be run.
+     */
+    Chip8(std::string romPath);
 };
